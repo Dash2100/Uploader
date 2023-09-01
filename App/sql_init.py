c2aec682eec1e8813ea05701fddec6a18d380d14
Tony2100 <87257550+Dash2100@users.noreply.github.com>
2023-09-01 20:03:10 +0800

    a

--- App/sql_init.py
+++ App/sql_init.py
@@ -1,20 +1,21 @@
 import sqlite3
 import hashlib
 
-default_password = 'admin'
-
 def sqlinit():
     con = sqlite3.connect('database.db')
     cur = con.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS "Files" ("name" TEXT, "date" TEXT, "size" TEXT,"share" INTEGER,"sharedate" TEXT, "downloads" INTEGER)')
     cur.execute('CREATE TABLE IF NOT EXISTS "ShortUrls" ("url" TEXT, "file" TEXT)')
     cur.execute('CREATE TABLE IF NOT EXISTS "Users" ("username" TEXT, "password" TEXT)')
-
-    #check if admin user exists
+    #if no users exist, create admin user
     cur.execute("SELECT * FROM Users")
     users = cur.fetchall()
     if not users:
-        cur.execute("INSERT INTO Users VALUES (?, ?)", ('admin', hashlib.sha256(default_password.encode('utf-8')).hexdigest()))
+        #create admin user password = "admin"
+        username = 'admin'
+        password = "admin"
+        password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        cur.execute("INSERT INTO Users VALUES (?, ?)", (username, password_hash))
     con.commit()
     con.close()
 
