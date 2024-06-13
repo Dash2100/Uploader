baa64bb62e1f0bfc0b2483b36bced87c43acb4a0
Tony2100 <tony1381471821@gmail.com>
2024-06-14 04:59:09 +0800

    back

--- NewVersion/app/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_login import LoginManager
-
-login_manager = LoginManager()
-
-def init_login_manager(app):
-    login_manager.init_app(app)
-
-    login_manager.login_view = 'auth.login'
